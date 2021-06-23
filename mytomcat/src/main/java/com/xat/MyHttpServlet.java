47970621b11af061086cd196277461cde4f78883
antaoxu97@163.com <antaoxu97@163.com>
2021-06-23 18:21:15 +0800

    自定义tomcat将抽象类改为接口实现

--- mytomcat/src/main/java/com/xat/MyHttpServlet.java
+++ mytomcat/src/main/java/com/xat/MyHttpServlet.java
@@ -5,25 +5,20 @@ import java.io.IOException;
 /**
  * @author xuantao
  */
-public  abstract class MyHttpServlet {
+public interface MyHttpServlet {
     //定义静态常量
     public static final String METHOD_GET = "GET";
     public static final String METHOD_POST = "POST";
 
     //定义抽象方法供子类重写
-    public abstract void doGet(MyRequest request,MyResponse response) throws IOException;
-    public abstract void doPost(MyRequest request,MyResponse response) throws IOException;
+
+     void doGet(MyRequest request,MyResponse response) throws IOException;
+     void doPost(MyRequest request,MyResponse response) throws IOException;
 
     /**
      * 根据请求方式来判断执行哪种方法
      * @param request
      * @param response
      */
-    public void service(MyRequest request,MyResponse response) throws IOException {
-        if(METHOD_GET.equals(request.getRequestMethod())){
-            doGet(request,response);
-        }else if(METHOD_POST.equals(request.getRequestMethod())){
-            doPost(request,response);
-        }
-    }
+     void service(MyRequest request,MyResponse response) throws IOException;
 }
--- mytomcat/src/main/java/com/xat/MyServlet.java
+++ mytomcat/src/main/java/com/xat/MyServlet.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 /**
  * @author xuantao
  */
-public class MyServlet extends MyHttpServlet {
+public class MyServlet implements MyHttpServlet {
     @Override
     public void doGet(MyRequest request, MyResponse response) throws IOException {
         response.write("GET tomcat");
@@ -14,5 +14,19 @@ public class MyServlet extends MyHttpServlet {
     @Override
     public void doPost(MyRequest request, MyResponse response) throws IOException {
         response.write("POST tomcat");
+    }
+
+    /**
+     * 根据请求方式来判断执行哪种方法
+     * @param request
+     * @param response
+     */
+    @Override
+    public void service(MyRequest request, MyResponse response) throws IOException {
+        if(METHOD_GET.equals(request.getRequestMethod())){
+            doGet(request,response);
+        }else if(METHOD_POST.equals(request.getRequestMethod())){
+            doPost(request,response);
+        }
     }
 }
